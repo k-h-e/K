cf3796804f2da309e86edac41cc07f4782c076a6
Kai Hergenröther <kai@kais-mbp.speedport.ip>
2020-05-18 19:09:51 +0200

    Event handlers can now be registered with and unregistered from an event loop from the loop's event handlers.

--- src/Events/include/K/Events/EventLoop.h
+++ src/Events/include/K/Events/EventLoop.h
@@ -18,8 +18,8 @@ namespace Events {
 /*!
  *  The \ref EventLoop s of multiple threads are integrated using an \ref EventLoopHub.
  *
- *  <c>EventClass</c> must derive from <c>Event</c> and implement a method
- *  <c>void Dispatch(EventHandlerClass *handler) const</c>.
+ *  <c>EventClass</c> must derive from <c>Event</c> and implement a method <c>void Dispatch(EventHandlerClass *handler)
+ *  const</c>.
  *
  *  \ingroup Events
  */
@@ -37,10 +37,17 @@ class EventLoop : public virtual EventReceiverInterface {
     //! Registers a handler for the specified event, as a weak reference.
     /*!
      *  A given handler must be registered for a given event only once.
+     *
+     *  May get called from event handlers invoked by the loop.
      */
     void RegisterHandler(const Event::EventType &eventType, EventHandlerClass *handler);
-    //! Unregisters all event handlers.
-    void UnregisterHandlers();
+    //! Unregisters the specified event handler.
+    /*!
+     *  When this method returns, it is guaranteed that the loop will not call the handler again.
+     *
+     *  May get called from event handlers invoked by the loop.
+     */
+    void UnregisterHandler(EventHandlerClass *handler);
     //! Runs the event loop until shutdown is requested (via the hub).
     void Run();
     //! Runs the event loop until the next event of the specified type has been dispatched.
@@ -57,8 +64,7 @@ class EventLoop : public virtual EventReceiverInterface {
     bool RunBatch();
     //! Posts the specified event for execution on the loop.
     /*!
-     *  This is the only method that may be called while any \ref Run() method executes, that is: from event handlers
-     *  invoked by the loop.
+     *  May get called from event handlers invoked by the loop.
      */
     virtual void Post(const Event &event);
         
@@ -71,13 +77,15 @@ class EventLoop : public virtual EventReceiverInterface {
     
     EventClass *DispatchOne();
 
-    std::vector<EventInfo>          events_;
-    std::unordered_map<size_t, int> idToSlotMap_;
-    std::unique_ptr<Core::Buffer>   eventsToDispatch_;
-    Core::Buffer::Reader            reader_;
-    std::shared_ptr<EventLoopHub>   hub_;
-    int                             hubClientId_;
-    bool                            running_;
+    std::vector<EventInfo>           events_;
+    std::unordered_map<size_t, int>  idToSlotMap_;
+    std::unique_ptr<Core::Buffer>    eventsToDispatch_;
+    Core::Buffer::Reader             reader_;
+    std::vector<EventHandlerClass *> pendingHandlers_;
+    int                              pendingHandlerCursor_;
+    std::shared_ptr<EventLoopHub>    hub_;
+    int                              hubClientId_;
+    bool                             running_;
 };
 
 template<class EventClass, class EventHandlerClass>
@@ -110,21 +118,38 @@ void EventLoop<EventClass, EventHandlerClass>::RegisterEvent(std::unique_ptr<Eve
 
 template<class EventClass, class EventHandlerClass>
 void EventLoop<EventClass, EventHandlerClass>::RegisterHandler(const Event::EventType &eventType,
-                                                               EventHandlerClass *handlerToRegister) {
-    assert(!running_);
+                                                               EventHandlerClass *handler) {
     auto infoIter = idToSlotMap_.find(eventType.id);
     assert(infoIter != idToSlotMap_.end());
-    for (EventHandlerClass *handler : events_[infoIter->second].handlers) {
-        assert(handler != handlerToRegister);
+    for (EventHandlerClass *aHandler : events_[infoIter->second].handlers) {
+        assert(aHandler != handler);
     }
-    events_[infoIter->second].handlers.push_back(handlerToRegister);
+    events_[infoIter->second].handlers.push_back(handler);
 }
 
 template<class EventClass, class EventHandlerClass>
-void EventLoop<EventClass, EventHandlerClass>::UnregisterHandlers() {
-    assert(!running_);
+void EventLoop<EventClass, EventHandlerClass>::UnregisterHandler(EventHandlerClass *handler) {
     for (EventInfo &info : events_) {
+        std::vector<EventHandlerClass *> oldHandlers(info.handlers);
         info.handlers.clear();
+        for (EventHandlerClass *aHandler : oldHandlers) {
+            if (aHandler != handler) {
+                info.handlers.push_back(aHandler);
+            }
+        }
+    }
+
+    // In case we were called from one of our own event handlers, we must make sure the unregistered handler will no
+    // longer be called in the process of the remaining event dispatch.
+    if (!pendingHandlers_.empty()) {
+        std::vector<EventHandlerClass *> oldPendingHandlers(pendingHandlers_);
+        pendingHandlers_.clear();
+        for (int i = 0; i < static_cast<int>(oldPendingHandlers.size()); ++i) {
+            EventHandlerClass *aHandler = oldPendingHandlers[i];
+            if ((i <= pendingHandlerCursor_) || (aHandler != handler)) {
+                pendingHandlers_.push_back(aHandler);
+            }
+        }
     }
 }
 
@@ -202,9 +227,22 @@ EventClass *EventLoop<EventClass, EventHandlerClass>::DispatchOne() {
         EventInfo  &info  = events_[slot];
         EventClass &event = *(info.prototype);
         event.Deserialize(&reader_);
+
+        // We allow handlers to be registered and deregistered from event handlers, so we copy the list of handlers for
+        // the current event before beginning to dispatch it. Wenn a handler is unregistered during dispatch, it also
+        // gets removed from this copied list of pending handlers.
+        pendingHandlers_.clear();
         for (EventHandlerClass *handler : info.handlers) {
-            event.Dispatch(handler);
+            pendingHandlers_.push_back(handler);
         }
+        pendingHandlerCursor_ = 0;
+        while (pendingHandlerCursor_ < static_cast<int>(pendingHandlers_.size())) {
+            EventHandlerClass *handler = pendingHandlers_[pendingHandlerCursor_];
+            event.Dispatch(handler);
+            ++pendingHandlerCursor_;
+        }
+        pendingHandlers_.clear();
+
         return &event;
     }
     else {
