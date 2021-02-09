37c9ac6f6210d7d568734806f4e47bf919e45a5c
Kai Hergenroether <kai@hergenrother.de>
2021-02-10 00:01:29 +0100

    - Added completion tracker.

---
+++ src/Core/CompletionTracker.cpp
@@ -0,0 +1,23 @@
+#include <K/Core/CompletionTracker.h>
+
+using std::mutex;
+using std::unique_lock;
+
+namespace K {
+namespace Core {
+
+void CompletionTracker::WaitForCompletion(int completionId) {
+    unique_lock<mutex> critical(lock_);    // Critical section..........................................................
+    while (setCompletionStates_.find(completionId) == setCompletionStates_.end()) {
+        stateChanged_.wait(critical);
+    }
+}    // ......................................................................................... critical section, end.
+
+void CompletionTracker::OnCompletion(int completionId) {
+    unique_lock<mutex> critical(lock_);    // Critical section..........................................................
+    setCompletionStates_.insert(completionId);
+    stateChanged_.notify_all();
+}
+
+}    // Namespace Core.
+}    // Namespace K.
---
+++ src/Core/include/K/Core/CompletionTracker.h
@@ -0,0 +1,37 @@
+#ifndef K_CORE_COMPLETIONTRACKER_H_
+#define K_CORE_COMPLETIONTRACKER_H_
+
+#include <mutex>
+#include <condition_variable>
+#include <unordered_set>
+#include <K/Core/CompletionHandlerInterface.h>
+
+namespace K {
+namespace Core {
+
+//! Keeps track of completion states.
+/*!
+ *  The class is thread-safe, i.e. all its public instance methods are.
+ */
+class CompletionTracker : public virtual CompletionHandlerInterface {
+  public:
+    CompletionTracker()                                          = default;
+    CompletionTracker(const CompletionTracker &other)            = delete;
+    CompletionTracker &operator=(const CompletionTracker &other) = delete;
+    CompletionTracker(CompletionTracker &&other)                 = delete;
+    CompletionTracker &operator=(CompletionTracker &&other)      = delete;
+
+    //! Blocks until the specified completion state has been set.
+    void WaitForCompletion(int completionId);
+    void OnCompletion(int completionId) override;
+
+  private:
+    std::mutex              lock_;
+    std::condition_variable stateChanged_;
+    std::unordered_set<int> setCompletionStates_;
+};
+
+}    // Namespace Core.
+}    // Namespace K.
+
+#endif    // K_CORE_COMPLETIONTRACKER_H_
